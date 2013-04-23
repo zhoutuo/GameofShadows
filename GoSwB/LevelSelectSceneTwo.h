40e86b02ea7ecce0c64faa0a6f05b26888157169
Ryan Ball <ryanball@Ryans-MacBook-Air.local>
2013-04-22 21:58:39 -0700

    Added all the level images and more levels

---
+++ GoSwB/LevelSelectSceneTwo.h
@@ -0,0 +1,13 @@
+//
+//  LevelSelectSceneTwo.h
+//  Game of Shadows
+//
+//  Created by Ryan Ball on 4/22/13.
+//
+//
+
+#import "CCScene.h"
+
+@interface LevelSelectSceneTwo : CCLayer
+
+@end
--- GoSwB/LevelSelectSceneTwo.mm
+++ GoSwB/LevelSelectSceneTwo.mm
@@ -1,73 +1,73 @@
 //
-//  LevelSelectScene.m
+//  LevelSelectScene2.m
 //  Shadow UI
 //
 //  Created by Ryan Ball on 2/18/13.
 //
 //
 
-#import "LevelSelectScene.h"
+#import "LevelSelectSceneTwo.h"
 #import "CCBReader.h"
 #import "CCMenu.h"
 #import "Globals.h"
 #import "GameplayScene.h"
 
-#define LEVEL_1_1_TAG 1
-#define LEVEL_1_2_TAG 2
-#define LEVEL_1_3_TAG 3
-#define LEVEL_1_4_TAG 4
-#define LEVEL_1_5_TAG 5
-#define LEVEL_1_6_TAG 6
-#define LEVEL_1_7_TAG 7
-#define LEVEL_1_8_TAG 8
+#define LEVEL_2_1_TAG 1
+#define LEVEL_2_2_TAG 2
+#define LEVEL_2_3_TAG 3
+#define LEVEL_2_4_TAG 4
+#define LEVEL_2_5_TAG 5
+#define LEVEL_2_6_TAG 6
+#define LEVEL_2_7_TAG 7
+#define LEVEL_2_8_TAG 8
 #define BACK_BUTTON_TAG 10
 #define FORWARD_BUTTON_TAG 11
 #define HOME_BUTTON_TAG 20
 
-@implementation LevelSelectScene
+@implementation LevelSelectSceneTwo
 
 -(void)buttonPressed:(id)sender {
     CCMenuItem *button = (CCMenuItem*) sender;
     switch (button.tag) {
             //Temporarily all point back to main menu
+            //CURRENT LEVEL INCREMENTED BY 10 FROM 1st level select
+        case LEVEL_2_1_TAG:
+            currentLevel = 11;
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
+            break;
             
-        case LEVEL_1_1_TAG:
-            currentLevel = 1;
+        case LEVEL_2_2_TAG:
+            currentLevel = 12;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-
-        case LEVEL_1_2_TAG:
-            currentLevel = 2;
+        case LEVEL_2_3_TAG:
+            currentLevel = 13;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-        case LEVEL_1_3_TAG:
-            currentLevel = 3;
+        case LEVEL_2_4_TAG:
+            currentLevel = 14;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-        case LEVEL_1_4_TAG:
-            currentLevel = 4;
+        case LEVEL_2_5_TAG:
+            currentLevel = 15;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-        case LEVEL_1_5_TAG:
-            currentLevel = 5;
+        case LEVEL_2_6_TAG:
+            currentLevel = 16;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-        case LEVEL_1_6_TAG:
-            currentLevel = 6;
+        case LEVEL_2_7_TAG:
+            currentLevel = 17;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
             break;
-        case LEVEL_1_7_TAG:
-            currentLevel = 7;
+        case LEVEL_2_8_TAG:
+            currentLevel = 18;
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
-            break;
-        case LEVEL_1_8_TAG:
-            currentLevel = 8;
-            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[GameplayScene node]]];
-            break;
-        case FORWARD_BUTTON_TAG:
-            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[CCBReader sceneWithNodeGraphFromFile:@"LevelSelectSceneTwo.ccbi"]]];
             break;
         case BACK_BUTTON_TAG:
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[CCBReader sceneWithNodeGraphFromFile:@"LevelSelectScene.ccbi"]]];
+            break;
+        case FORWARD_BUTTON_TAG:
         case HOME_BUTTON_TAG:
             [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5 scene:[CCBReader sceneWithNodeGraphFromFile:@"MainMenuScene.ccbi"]]];
             break;
@@ -76,3 +76,4 @@
 }
 
 @end
+
